67162dea5d70dcf0f5427440b6cd60dc9887a230
sebbejohansson <sebbe.johansson1995@gmail.com>
2021-07-19 16:28:00 +0200

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<i18n> 
+{   
+  "en": {     
+    "title": "What Came First?",
+    "about": "The game about trying to figure out what came first."   
+  }
+} 
+</i18n>
+
+<template>
+  <div class="index">
+    <div class="index__intro">
+      <h1 class="index__title">
+        {{ $t('title') }}
+      </h1>
+      <p class="index__about">
+        {{ $t('about') }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, watch } from '@nuxtjs/composition-api';
+  
+  export default defineComponent({
+
+  });
+</script>
+
+<style>
+
+.index__title{
+  font-family: Qahiri,sans-serif;
+  font-size: 6rem;
+  line-height: 6rem;
+  margin: 0;
+}
+.index__about{
+  font-family: 'Montserrat', sans-serif;
+  margin: 0;
+}
+
+
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    {{ $t('message') }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  nuxtI18n: {
+    paths: {
+      en: '/test',
+      sv: '/svenskttest',
+    }
+  }
+})
+</script>
+
+<style>
+</style>
